9d51dd20d88ecbcdb334a72cb49cfe283f691800
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-09-30 19:30:44 +0200

    Add debug toolbar to urlpattern

--- cotal_drf_api/urls.py
+++ cotal_drf_api/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += path('__debug__/', include('debug_toobar.urls'))
