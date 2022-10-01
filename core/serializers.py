ffd0418805e08b0b15fc72ed9431970335b1e360
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 20:08:03 +0200

    Refactor serializers to include social links independently

--- core/serializers.py
+++ core/serializers.py
@@ -1,15 +1,30 @@
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from .models import *
 
 
-class ProfileLinkSerializer(serializers.Serializer):
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+
+
+class ProfileSocialLink(serializers.ModelSerializer):
+    class Meta:
+        model = SocialUsername
+        fields = ['id', 'social_media', 'social_username']
+
+
+class ProfileLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfileLink
-        fields = ['id', 'external', 'social']
+        fields = ['id', 'external', 'social_id', 'social_links']
+
+    social_links = ProfileSocialLink()
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'image', 'status', 'link']
-        
+        fields = ['id', 'user_id', 'image', 'status', 'location', 'link_id', 'profile_links']
+
+    profile_links = ProfileLinkSerializer()
