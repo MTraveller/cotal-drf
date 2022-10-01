9653bd66c5b34932f78620e01110dfe7697e2895
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 18:10:24 +0200

    Rename userprofile path to users & include core app

--- cotal_drf_api/urls.py
+++ cotal_drf_api/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('userprofile/', include('userprofile.urls')),
+    path('users/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
 
