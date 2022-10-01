90d95c5346d10a2077e5a7cf153e23a668bb0884
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 15:35:42 +0200

    Add Templates dir

--- cotal_drf_api/settings/common.py
+++ cotal_drf_api/settings/common.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'cotal_drf_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
