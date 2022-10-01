57a49a0e636657dd6b52d1fc0bb84716a43a937e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 18:29:39 +0200

    Add djoser & jwt paths

--- cotal_drf_api/urls.py
+++ cotal_drf_api/urls.py
@@ -19,7 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('users/', include('core.urls')),
+    path('core/', include('core.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('admin/', admin.site.urls),
 ]
 
