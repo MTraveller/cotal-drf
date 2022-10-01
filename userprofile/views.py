cbb10a52756b6ba0b1b993275157df1e0cc04e9b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 14:45:54 +0200

    Initial userprofile profile list & detail views

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+@api_view()
+def profile_list(request):
+    queryset = Profile.objects.select_related('link').all()
+    serializer = ProfileSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def profile_detail(request, id):
+    profile = get_object_or_404(Profile, pk=id)
+    serializer = ProfileSerializer(profile)
+    return Response(serializer.data)
