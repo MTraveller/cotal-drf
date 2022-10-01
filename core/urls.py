315fb769bf854857641d1fa0dde2e83bc78f98db
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-01 20:09:17 +0200

    Define & Add router

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register('profiles', views.ProfileViewSet)
+router.urls
+
 # URLConf
-urlpatterns = [
-    path('profiles/', views.profile_list),
-    path('profiles/<int:id>', views.profile_detail),
-]
+urlpatterns = router.urls
