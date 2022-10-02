6b592f588c05698447786a5ffc42d7a0d178974e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-02 18:13:06 +0200

    Add link and social viewsets & cleaup imports

--- core/views.py
+++ core/views.py
@@ -1,32 +1,30 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.response import Response
-from .models import Profile
-from .serializers import ProfileSerializer
-
-
-# @api_view(['GET', 'POST'])
-# def profile_list(request):
-#     if request.method == 'GET':
-#         queryset = Profile.objects.select_related('link').all()
-#         serializer = ProfileSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = ProfileSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response('OK')
-
-
-# @api_view()
-# def profile_detail(request, id):
-#     profile = get_object_or_404(Profile, pk=id)
-#     serializer = ProfileSerializer(profile)
-#     return Response(serializer.data)
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from .models import Profile, Link, Social
+from .serializers import ProfileSerializer, ProfileLinkSerializer, ProfileSocialSerializer
 
 
 class ProfileViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class LinkViewSet(ModelViewSet):
+    serializer_class = ProfileLinkSerializer
+
+    def get_queryset(self):
+        return Link.objects.filter(profile_id=self.kwargs['profile_pk'])
+
+    def get_serializer_context(self):
+        return {'profile_id': self.kwargs['profile_pk']}
+
+
+class SocialViewSet(ModelViewSet):
+    serializer_class = ProfileSocialSerializer
+
+    def get_queryset(self):
+        return Social.objects.filter(profile_id=self.kwargs['profile_pk'])
+
+    def get_serializer_context(self):
+        return {'profile_id': self.kwargs['profile_pk']}
+        
