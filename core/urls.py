1341ea1f7700c1d346285709d6317871a34a127d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-02 18:10:23 +0200

    Fix view set name for socials route

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ router.register('profiles', views.ProfileViewSet)
 
 profiles_router = routers.NestedDefaultRouter(router, 'profiles', lookup='profile')
 profiles_router.register('links', views.LinkViewSet, basename='profile-links')
-profiles_router.register('socials', views.LinkViewSet, basename='profile-socials')
+profiles_router.register('socials', views.SocialViewSet, basename='profile-socials')
 
 # URLConf
 urlpatterns = router.urls + profiles_router.urls
