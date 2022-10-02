cc4ff9fa070313b6c04de70effd76c6543a5423d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-02 20:54:54 +0200

    Rename profiles to profile

--- user/urls.py
+++ user/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from rest_framework_nested import routers
 from . import views
 
+# Guide source:
+# https://github.com/alanjds/drf-nested-routers#quickstart
 router = routers.DefaultRouter()
 router.register('profile', views.ProfileViewSet)
 
-profiles_router = routers.NestedDefaultRouter(
+profile_router = routers.NestedDefaultRouter(
     router, 'profile', lookup='profile')
-profiles_router.register('links', views.LinkViewSet, basename='profile-links')
-profiles_router.register('socials', views.SocialViewSet,
-                         basename='profile-socials')
+profile_router.register('links', views.LinkViewSet, basename='profile-links')
+profile_router.register('socials', views.SocialViewSet,
+                        basename='profile-socials')
 
 # URLConf
-urlpatterns = router.urls + profiles_router.urls
+urlpatterns = router.urls + profile_router.urls
