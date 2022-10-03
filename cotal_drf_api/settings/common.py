2aad98760bddaa449c34bbeebae935aff7fbddd7
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-03 08:16:25 +0200

    Add IsAuthenticated setting

--- cotal_drf_api/settings/common.py
+++ cotal_drf_api/settings/common.py
@@ -119,6 +119,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 AUTH_USER_MODEL = 'user.User'
