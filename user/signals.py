286e3052c87821110d10ac191e959ec9b79a507c
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-03 09:55:35 +0200

    Add delete signal on profile delete

--- user/signals.py
+++ user/signals.py
@@ -1,8 +1,9 @@
 """ User Signals """
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Profile
+from rest_framework.response import Response
+from .models import Profile, User
 
 
 USER = settings.AUTH_USER_MODEL
@@ -12,3 +13,12 @@ USER = settings.AUTH_USER_MODEL
 def create_profile(sender, instance, **kwargs):
     """ Signal to create profile on user creation """
     Profile.objects.create(user=instance)
+
+
+@receiver(post_delete, sender=Profile)
+def delete_user(sender, instance, **kwargs):
+    """ Signal to delete user on profile deletion """
+    user = instance.user
+    user.delete()
+
+    return Response("Deleted")
