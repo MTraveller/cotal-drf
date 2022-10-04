8edaf2efb3d5c100220e58194b1ba8805761a6d6
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-04 18:02:58 +0200

    Remove debug_toolbar from installed apps

--- cotal_drf_api/settings/common.py
+++ cotal_drf_api/settings/common.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
-    'debug_toolbar',
     'django_filters',
     'rest_framework',
     'djoser',
