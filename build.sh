d28f908e65e120c33b818d3b55bd02b18cfee4d1
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-04 16:54:13 +0200

    Add if web run migrate

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
-python manage.py migrate
+set -e # exit on error
+
+pip3 install -r requirements.txt
+
+if [ "$RENDER_SERVICE_TYPE" = "web" ]; then
+  python manage.py migrate --no-input
+fi
+
 python manage.py collectstatic
