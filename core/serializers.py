7f9d063f7fcc25795b1ed86ebb59dfb369dedaf8
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-05 18:07:15 +0200

    Add remaining models in core models file

--- core/serializers.py
+++ core/serializers.py
@@ -40,14 +40,71 @@ class ProfileLinkSerializer(serializers.ModelSerializer):
         return Link.objects.create(profile_id=profile_id, **validated_data)
 
 
+class ProfilePortfolioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Portfolio
+        fields = ['id', 'image', 'title', 'description', 'link']
+
+    def create(self, validated_data):
+        profile_id = self.context['profile_id']
+        return Portfolio.objects.create(profile_id=profile_id, **validated_data)
+
+
+class ProfileAwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = ['id', 'image', 'title', 'description', 'link']
+
+    def create(self, validated_data):
+        profile_id = self.context['profile_id']
+        return Award.objects.create(profile_id=profile_id, **validated_data)
+
+
+class ProfileCertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ['id', 'image', 'title', 'description', 'link']
+
+    def create(self, validated_data):
+        profile_id = self.context['profile_id']
+        return Certificate.objects.create(profile_id=profile_id, **validated_data)
+
+
+class ProfileCreativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Creative
+        fields = ['id', 'title', 'description', 'link']
+
+    def create(self, validated_data):
+        profile_id = self.context['profile_id']
+        return Creative.objects.create(profile_id=profile_id, **validated_data)
+
+
+class ProfileSettingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Setting
+        fields = []
+
+    def create(self, validated_data):
+        profile_id = self.context['profile_id']
+        return Setting.objects.create(profile_id=profile_id, **validated_data)
+
+
 class ProfileSerializer(serializers.ModelSerializer):
     # https://www.django-rest-framework.org/api-guide/relations/#nested-relationships
     links = ProfileLinkSerializer(many=True, read_only=True)
     socials = ProfileSocialSerializer(many=True, read_only=True)
+    portfolio = ProfilePortfolioSerializer(many=True, read_only=True)
+    award = ProfileAwardSerializer(many=True, read_only=True)
+    certificate = ProfileCertificateSerializer(many=True, read_only=True)
+    creative = ProfileCreativeSerializer(many=True, read_only=True)
+    setting = ProfileSettingSerializer(many=True, read_only=True)
 
     class Meta:
         model = Profile
         fields = [
             'user_id', 'image', 'status',
-            'location', 'links', 'socials'
+            'location', 'links', 'socials',
+            'portfolio', 'award', 'certificate',
+            'creative', 'setting'
         ]
