dcd695dae41badd163b7c7b52a108e14110a295f
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-06 15:49:56 +0200

    Add no create permission

--- core/permissions.py
+++ core/permissions.py
@@ -20,3 +20,15 @@ class IsObjectUser(permissions.BasePermission):
         if request.user and request.user.id == obj.id:
             return True
         return IsObjectUser.has_permission
+
+
+class IsNotObjectUserOrReadOnly(permissions.BasePermission):
+    """
+    Custom no create permission based on rest_framework BasePermission
+    """
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return bool(request.method in permissions.SAFE_METHODS)
