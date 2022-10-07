c0d4cc2619ed4d3af6fc7b94abd16b9b5d810103
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-07 11:07:56 +0200

    Add media uploaded & instance deleted signals

--- core/signals/__init__.py
+++ core/signals/__init__.py
@@ -3,3 +3,9 @@ from django.dispatch import Signal
 
 # Signal for user created
 user_created = Signal()
+
+# Signal for media uploaded
+media_uploaded = Signal()
+
+# Signal for instance deleted
+instance_deleted = Signal()
