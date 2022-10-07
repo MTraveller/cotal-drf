af7ad11e6230725d9e458fa0f2cdb022a2606873
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-07 11:12:41 +0200

    Add signal functions to instance delete images

--- core/signals/handlers.py
+++ core/signals/handlers.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from rest_framework.response import Response
+import cloudinary
 from ..models import Profile
+from . import media_uploaded, instance_deleted
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -17,5 +19,17 @@ def delete_user(sender, instance, **kwargs):
     """ Signal to delete user on profile deletion """
     user = instance.user
     user.delete()
+    cloudinary.uploader.destroy(str(user))
 
     return Response("User has been successfully deleted")
+
+
+@receiver(media_uploaded)
+def delete_previous_image_cloudinary(sender, **kwargs):
+    """ Signal to delete previous image on cloudinary """
+    cloudinary.uploader.destroy(str(kwargs['image']))
+
+
+@receiver(instance_deleted)
+def delete_instance_image_cloudinary(sender, **kwargs):
+    cloudinary.uploader.destroy(str(kwargs['image']))
