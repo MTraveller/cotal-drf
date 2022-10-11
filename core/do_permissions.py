e14e5075ead95f76e73043c5fe522b5fdafc6339
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-11 15:05:50 +0200

    Add support for profiles app

--- core/do_permissions.py
+++ core/do_permissions.py
@@ -12,18 +12,26 @@ def try_match(self):
 
     if self.request.user.is_authenticated:
         profile_id = 0
-        match basename:
-            case 'profile-posts-list':
-                profile_id = kwargs['profile_pk']
-            case 'profile-posts-detail':
-                profile_id = kwargs['profile_pk']
-            case 'post-comments-list':
-                if self.request.user and self.request.user.is_authenticated:
-                    return [IsAuthenticated()]
-                else:
-                    return False
-            case 'post-comments-detail':
-                profile_id = self.__dict__['comment_profile_id'].profile_id
+        if basename.startswith('profile-'):
+            basename_list = basename.endswith('-list')
+            basename_detail = basename.endswith('-detail')
+            match basename_list or basename_detail:
+                case basename:
+                    profile_id = kwargs['profiles_pk']
+
+        elif basename.startswith('profiles-'):
+            match basename:
+                case 'profile-posts-list':
+                    profile_id = kwargs['profile_pk']
+                case 'profile-posts-detail':
+                    profile_id = kwargs['profile_pk']
+                case 'post-comments-list':
+                    if self.request.user and self.request.user.is_authenticated:
+                        return [IsAuthenticated()]
+                    else:
+                        return False
+                case 'post-comments-detail':
+                    profile_id = self.__dict__['comment_profile_id'].profile_id
 
         try:
             return bool(self.request.user.id == int(profile_id))
