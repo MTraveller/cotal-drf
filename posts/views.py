efa20f073fec738620a2ad0c4df8a09614edcf30
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-11 13:49:46 +0200

    Refactor & optimize querysets & pass profile_id to self in get_permissions()

--- posts/views.py
+++ posts/views.py
@@ -11,9 +11,9 @@ class ProfilePostViewSet(ModelViewSet):
     Profile post view set with appropiate permission handling.
     """
     queryset = Profile.objects \
-        .select_related('user__profile') \
+        .prefetch_related('user__profile') \
         .prefetch_related('profileposts__postimages') \
-        .prefetch_related('profileposts__postcomments') \
+        .prefetch_related('profileposts__postcomments__profile__user') \
         .all()
     serializer_class = ProfileSerializer
 
@@ -33,14 +33,9 @@ class PostViewSet(ModelViewSet):
     # https://docs.djangoproject.com/en/4.1/ref/models/querysets/#django.db.models.Prefetch
     def get_queryset(self):
         return Post.objects \
-            .prefetch_related(Prefetch(
-                'profile',
-                queryset=Profile.objects.select_related(
-                    'user'
-                )
-            )) \
-            .prefetch_related('postimages__profile') \
-            .prefetch_related('postcomments__profile') \
+            .prefetch_related('profile__user') \
+            .prefetch_related('postimages__profileposts') \
+            .prefetch_related('postcomments__profile__user') \
             .filter(profile_id=self.kwargs['profile_pk'])
 
     def get_serializer_context(self):
@@ -58,7 +53,7 @@ class PostImageViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PostImage.objects \
-            .select_related('post') \
+            .select_related('post__profile__user') \
             .filter(profile_id=self.kwargs['profiles_pk'])
 
     def get_serializer_context(self):
@@ -72,15 +67,28 @@ class PostCommentViewSet(ModelViewSet):
     serializer_class = PostCommentSerializer
 
     def get_permissions(self):
+        url_name = self.request.resolver_match.url_name
+        if url_name == 'post-comments-detail':
+            pk = self.request.resolver_match.kwargs['pk']
+            comment_profile_id = PostComment.objects \
+                .get(id=pk)
+            self.__dict__['comment_profile_id'] = comment_profile_id
         return do_permissions(self)
 
     def get_queryset(self):
+        url_name = self.request.resolver_match.url_name
+        if not url_name == 'post-comments-detail':
+            return PostComment.objects \
+                .prefetch_related('profile__user') \
+                .prefetch_related('post__profile__user') \
+                .filter(post_id=self.kwargs['post_pk'])
+        pk = self.__dict__['kwargs']['pk']
         return PostComment.objects \
-            .select_related('profile__user') \
-            .filter(post_id=self.kwargs['post_pk'])
+            .prefetch_related('profile__user') \
+            .filter(id=pk)
 
     def get_serializer_context(self):
         return {
             'post_id': self.kwargs['post_pk'],
-            'user_id': self.request.user.id,
+            'user_id': self.request.user.pk,
         }
