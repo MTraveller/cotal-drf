c6fc611b2a159c48addf5913437d14833030f891
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 21:21:16 +0200

    Add __str__ to return full name

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
