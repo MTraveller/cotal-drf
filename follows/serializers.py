aa4342fa6e4d6824467a63739e35f713d70b526e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 18:35:04 +0200

    Refactor followed by fields

--- follows/serializers.py
+++ follows/serializers.py
@@ -6,19 +6,19 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Followed
         fields = [
-            'id', 'follower_choice', 'followed_by_slug',
+            'id', 'follower_choice', 'followed_by_username',
         ]
         read_only_fields = [
-            'followed_by_slug'
+            'followed_by_username'
         ]
 
     def create(self, validated_data):
         profile_id = self.context['profile_id']
         followed_by_id = self.context['followed_by_id']
-        followed_by_slug = self.context['followed_by_slug']
+        followed_by_username = self.context['followed_by_username']
 
         return Followed.objects.create(
             profile_id=profile_id,
             followed_by_id=followed_by_id,
-            followed_by_slug=followed_by_slug,
+            followed_by_username=followed_by_username,
             **validated_data)
