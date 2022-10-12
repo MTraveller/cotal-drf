5c47818f2573c20b3e5bda70f723b52b89d90012
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 21:23:50 +0200

    Refactor fields after model refactoring

--- follows/serializers.py
+++ follows/serializers.py
@@ -6,19 +6,38 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Followed
         fields = [
-            'id', 'follower_choice', 'followed_by_username',
+            'follower_choice', 'followed_by_name',
+            'followed_by_username',
         ]
         read_only_fields = [
-            'followed_by_username'
+            'followed_by_username', 'followed_by_name',
         ]
 
     def create(self, validated_data):
         profile_id = self.context['profile_id']
         followed_by_id = self.context['followed_by_id']
+        followed_by_name = self.context['followed_by_name']
         followed_by_username = self.context['followed_by_username']
+        following_by_name = self.context['following_by_name']
+        following_by_username = self.context['following_by_username']
 
         return Followed.objects.create(
             profile_id=profile_id,
             followed_by_id=followed_by_id,
+            followed_by_name=followed_by_name,
             followed_by_username=followed_by_username,
-            **validated_data)
+            following_by_name=following_by_name,
+            following_by_username=following_by_username,
+            **validated_data
+        )
+
+
+class FollowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Followed
+        fields = [
+            'id', 'following_by_name', 'following_by_username',
+        ]
+        read_only_fields = [
+            'following_by_name', 'following_by_username',
+        ]
