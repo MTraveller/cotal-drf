3a5ed6172432e28f4bff6b2e7acc4ef4322495f7
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 18:31:03 +0200

    Add connecter username to connecter serializer & refactor separation of serializers

--- connects/serializers.py
+++ connects/serializers.py
@@ -6,16 +6,17 @@ class ConnecterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Connected
         fields = [
-            'id', 'connecter_choice', 'connecting_choice',
-            'connecter_id', 'connecting_id',
+            'id', 'connecter_choice',
         ]
 
     def create(self, validated_data):
         connecter_id = self.context['connecter_id']
         connecting_id = self.context['connecting_id']
+        connecter_username = self.context['connecter_username']
         return Connected.objects.create(
             connecter_id=connecter_id,
             connecting_id=connecting_id,
+            connecter_username=connecter_username,
             **validated_data)
 
 
@@ -23,8 +24,10 @@ class ConnectingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Connected
         fields = [
-            'id', 'connecter_choice', 'connecting_choice',
-            'connecter_id', 'connecting_id',
+            'id', 'connecting_choice', 'connecter_username',
+        ]
+        read_only_fields = [
+            'connecter_username'
         ]
 
     def update(self, instance, validated_data):
