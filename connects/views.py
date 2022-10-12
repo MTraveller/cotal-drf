8d7a7b7a6efcaa925a8d0e9088c4a78837a48fd5
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 03:06:40 +0200

    Add initial connecter & connecting viewsets

--- connects/views.py
+++ connects/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from core.do_permissions import do_permissions
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class ConnecterViewSet(ModelViewSet):
+    """
+    Profile view set with appropiate permission handling.
+    """
+    serializer_class = ConnecterSerializer
+
+    def get_permissions(self):
+        queryset = Connected.objects \
+            .filter(connecter_id=self.request.user.id)  # type: ignore
+        self.__dict__['queryset'] = list(queryset)
+        return do_permissions(self)
+
+    def get_queryset(self):
+        return Connected.objects \
+            .filter(connecting_id=self.kwargs['profiles_pk'])
+
+    def get_serializer_context(self):
+        return {
+            'connecter_id': self.request.user.id,  # type: ignore
+            'connecting_id': self.kwargs['profiles_pk']
+        }
+
+
+class ConnectingViewSet(ModelViewSet):
+    """
+    Profile view set with appropiate permission handling.
+    """
+    serializer_class = ConnectingSerializer
+
+    def get_permissions(self):
+        return do_permissions(self)
+
+    def get_queryset(self):
+        return Connected.objects \
+            .filter(connecting_id=self.request.user.id)  # type: ignore
+
+    def get_serializer_context(self):
+        basename = self.request.resolver_match.url_name
+        print(self.kwargs)
+        if basename == 'profile-connecting-detail':
+            return {
+                'id': self.kwargs['pk'],
+                'connecting_id': self.kwargs['profiles_pk']
+            }
+        return {
+            'connecting_id': self.kwargs['profiles_pk']
+        }
