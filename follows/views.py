58d08fba6a3dc67fdc4f41968e84c4aa88341fa4
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 21:25:05 +0200

    Refactor get_serializer_context() & add following viewset

--- follows/views.py
+++ follows/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from core.do_permissions import do_permissions
+from core.models import User
 from .serializers import *
 from .models import *
 
@@ -24,8 +25,30 @@ class FollowViewSet(ModelViewSet):
             .filter(profile_id=self.kwargs['profiles_pk'])
 
     def get_serializer_context(self):
+        following_by_name = User.objects \
+            .get(id=self.kwargs['profiles_pk'])
         return {
             'profile_id': self.kwargs['profiles_pk'],
             'followed_by_id': self.request.user.id,  # type: ignore
-            'followed_by_username': self.request.__dict__['_user'].username
+            'followed_by_name': self.request.user,
+            'followed_by_username': self.request.user.username,  # type: ignore
+            'following_by_name': following_by_name,
+            'following_by_username': following_by_name.username,
         }
+
+
+class FollowingViewSet(ModelViewSet):
+    """
+    Connecting view set with appropiate permission handling.
+    """
+    serializer_class = FollowingSerializer
+
+    def get_permissions(self):
+        queryset = Followed.objects \
+            .filter(followed_by_id=self.request.user.id)  # type: ignore
+        self.__dict__['queryset'] = list(queryset)
+        return do_permissions(self)
+
+    def get_queryset(self):
+        return Followed.objects \
+            .filter(followed_by_id=self.request.user.id)  # type: ignore
