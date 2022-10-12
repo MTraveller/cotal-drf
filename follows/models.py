78a1a3187e0779453938fc73c0e201d3c5032eac
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 15:52:56 +0200

    Add initial followed model with yes no choice

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from profiles.models import Profile
 
-# Create your models here.
+
+class Followed(models.Model):
+    profile = models.ForeignKey(Profile,
+                                on_delete=models.CASCADE, related_name='profilefollows')
+    following = models.ForeignKey(Profile,
+                                  on_delete=models.CASCADE, null=True, related_name='following')
+    followed = models.ForeignKey(Profile,
+                                 on_delete=models.CASCADE, null=True, related_name='followed')
+
+    class Follow(models.TextChoices):
+        NO = 0, _('0')
+        Yes = 1, _('1')
+
+    follower_choice = models.CharField(
+        max_length=1, choices=Follow.choices, default=Follow.NO)
