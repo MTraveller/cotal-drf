37ea545d802c46655c07d8539874828e31e9a74c
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-12 16:38:49 +0200

    Refactor fields names after model refactoring

--- follows/serializers.py
+++ follows/serializers.py
@@ -6,17 +6,19 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Followed
         fields = [
-            'id', 'follower_choice',
-            'following', 'followed',
+            'id', 'follower_choice', 'followed_by_slug',
         ]
         read_only_fields = [
-            'following', 'followed'
+            'followed_by_slug'
         ]
 
     def create(self, validated_data):
         profile_id = self.context['profile_id']
-        followed_id = self.context['followed_id']
+        followed_by_id = self.context['followed_by_id']
+        followed_by_slug = self.context['followed_by_slug']
+
         return Followed.objects.create(
             profile_id=profile_id,
-            followed_id=followed_id,
+            followed_by_id=followed_by_id,
+            followed_by_slug=followed_by_slug,
             **validated_data)
