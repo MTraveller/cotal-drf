b7800a93141ebc100797d56dc856cf863bd622d8
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-13 12:21:31 +0200

    Add initial tags app models

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=50)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.label)
+        super().save(*args, **kwargs)
+
+
+# https://docs.djangoproject.com/en/4.1/ref/contrib/contenttypes/#overview
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
