f68db632c493010e37d1f46fa2db5fab0bf4ef55
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-13 12:41:40 +0200

    Add tagged item custom manager

--- tags/models.py
+++ tags/models.py
@@ -4,6 +4,19 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.template.defaultfilters import slugify
 
 
+# https://docs.djangoproject.com/en/4.1/topics/db/managers/#custom-managers
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=50)
     slug = models.SlugField()
@@ -15,6 +28,7 @@ class Tag(models.Model):
 
 # https://docs.djangoproject.com/en/4.1/ref/contrib/contenttypes/#overview
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
