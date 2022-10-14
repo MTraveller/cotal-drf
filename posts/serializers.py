907095eeffb8b6b969ca6e9192d1c3f0cac1dfaf
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-14 15:50:11 +0200

    Add tags serializer relationship & customize create() to include tags

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,9 @@
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from core.serializers import BaseProfileSerializer, ProfileUserSerializer
+from tags.serializers import TaggedSerializer
+from tags.models import Tag, TaggedItem
 from .models import *
 
 
@@ -33,21 +37,63 @@ class PostSerializer(serializers.ModelSerializer):
     profile = BaseProfileSerializer(read_only=True)
     postimages = PostImageSerializer(many=True, required=False)
     postcomments = PostCommentSerializer(many=True, read_only=True)
+    tags = TaggedSerializer(many=True, required=False)
+
+    add_tags = serializers.ListField(
+        child=serializers.CharField(
+            max_length=50,
+            allow_blank=True,
+            trim_whitespace=True,
+        ),
+        required=False,
+        write_only=True,
+    )
+
+    title = serializers.CharField(trim_whitespace=True)
 
     class Meta:
         model = Post
         fields = ['id', 'profile', 'title', 'slug',
-                  'post', 'postimages', 'postcomments']
+                  'post', 'tags', 'add_tags', 'postimages', 'postcomments']
         read_only_fields = ['slug']
 
     def create(self, validated_data):
         profile_id = self.context['profile_id']
-        post = Post.objects.create(profile_id=profile_id, **validated_data)
-        if 'postimages' in validated_data:
-            images_data = validated_data.pop('postimages')
+        data = validated_data.copy()
+        print(data)
+        if ('add_tags' or 'postimages') in validated_data:
+            [validated_data.pop(key)
+             for key in ['tags', 'add_tags', 'postimages']]
+        post = Post.objects \
+            .create(profile_id=profile_id, **validated_data)
+
+        if 'add_tags' in data \
+                and bool(len(data['add_tags'])):
+            tags_data = data.pop('add_tags')
+            print(tags_data)
+            tags_data = [tag.lower() for tag in tags_data]
+            content_type_id = ContentType.objects.get(model="post").id
+
+            for label in tags_data:
+                tag = ""
+                try:
+                    tag = Tag.objects.get(label=label)
+                except ObjectDoesNotExist:
+                    tag = Tag.objects.create(label=label)
+
+                TaggedItem.objects.create(
+                    object_id=post.id,  # type: ignore
+                    content_type_id=content_type_id,  # type: ignore
+                    tag_id=tag.id  # type: ignore
+                )
+
+        if 'postimages' in data \
+                and bool(len(data['postimages'])):
+            images_data = data.pop('postimages')
             for image_data in images_data:
                 PostImage.objects.create(
                     profile_id=profile_id, post=post, **image_data)
+
         return post
 
 
