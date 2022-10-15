0f69a21ae941a4f770b9b54d76957f284d71bc5e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-15 10:41:45 +0200

    Refactor: add better readability to connect subclass choices

--- connects/models.py
+++ connects/models.py
@@ -5,6 +5,11 @@ from profiles.models import Profile
 
 
 class Connected(models.Model):
+    """
+    Connected model to connect two profiles, while
+    the connecter initiates the connection and the
+    connecting either accepts the connection or igores it.
+    """
     connecter = models.ForeignKey(
         Profile, on_delete=models.CASCADE, related_name='connecters')
     connecting = models.ForeignKey(
@@ -13,13 +18,14 @@ class Connected(models.Model):
     connecter_username = models.CharField(max_length=150)
 
     class Connect(models.TextChoices):
-        NO = 0, _('0')
-        Yes = 1, _('1')
+        IGNORE = 0, _('0')
+        CONNECT = 1, _('1')
+        UNSPECIFIED = 2, _('2')
 
     connecter_choice = models.CharField(
-        max_length=1, choices=Connect.choices, default=Connect.NO)
+        max_length=1, choices=Connect.choices, default=Connect.UNSPECIFIED)
     connecting_choice = models.CharField(
-        max_length=1, choices=Connect.choices, default=Connect.NO)
+        max_length=1, choices=Connect.choices, default=Connect.UNSPECIFIED)
 
     def save(self, *args, **kwargs):
         profile_connect.send_robust(
