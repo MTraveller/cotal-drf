f4bfdd24bc469c100c65486d703a0f550af61ca9
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-15 16:41:18 +0200

    Refactor: triple nested for loop with list comprehension & Add: serializers class docstring

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,10 @@ from .models import *
 
 
 class PostCommentSerializer(serializers.ModelSerializer):
+    """
+    Post comment serializer to handle post comments
+    1 level deep.
+    """
     profile = BaseProfileSerializer(read_only=True)
 
     class Meta:
@@ -27,6 +31,9 @@ class PostCommentSerializer(serializers.ModelSerializer):
 
 
 class PostImageSerializer(serializers.ModelSerializer):
+    """
+    Post image serializer controlling post images.
+    """
     class Meta:
         model = PostImage
         fields = ['id', 'image']
@@ -39,6 +46,10 @@ class PostImageSerializer(serializers.ModelSerializer):
 
 # https://www.django-rest-framework.org/api-guide/relations/#writable-nested-serializers
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Post serializer extends profile, postimages,
+    postcomments and tagged serializers
+    """
     profile = BaseProfileSerializer(read_only=True)
     postimages = PostImageSerializer(many=True, required=False)
     postcomments = PostCommentSerializer(many=True, read_only=True)
@@ -67,9 +78,17 @@ class PostSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         profile_id = self.context['profile_id']
-        data = validated_data.copy()
-        print(data)
+
+        # filter with lambda to extract
+        # targeted dicts from validated data
+        data = dict(filter(
+            lambda key: key[0] in [
+                'tags', 'add_tags', 'postimages'
+            ], validated_data.items()))
+
         if ('add_tags' or 'postimages') in validated_data:
+            # List comprehension to remove targeted dicts
+            # from validated data.
             [validated_data.pop(key)
              for key in ['tags', 'add_tags', 'postimages']]
         post = Post.objects \
@@ -78,9 +97,8 @@ class PostSerializer(serializers.ModelSerializer):
         if 'add_tags' in data \
                 and bool(len(data['add_tags'])):
             tags_data = data.pop('add_tags')
-            print(tags_data)
             tags_data = [tag.lower() for tag in tags_data]
-            content_type_id = ContentType.objects.get(model="post").id
+            content_type = ContentType.objects.get(model="post")
 
             for label in tags_data:
                 tag = ""
@@ -91,7 +109,7 @@ class PostSerializer(serializers.ModelSerializer):
 
                 TaggedItem.objects.create(
                     object_id=post.id,  # type: ignore
-                    content_type_id=content_type_id,  # type: ignore
+                    content_type=content_type,  # type: ignore
                     tag_id=tag.id  # type: ignore
                 )
 
@@ -107,18 +125,25 @@ class PostSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         updated_tags = validated_data['tags']
 
-        validated_tags = []
-        for dict in updated_tags:
-            for inner_dict in dict.values():
-                for tag in inner_dict.values():
-                    validated_tags.append(tag)
+        # List comprehension to extract tags
+        # within triple nested OrderedDicts.
+        validated_tags = [
+            list(list(tag)[0] for tag in (
+                inner_dict_v.values()
+                for key, inner_dict_v in outer_dict.items()
+            ))[0] for outer_dict in updated_tags
+        ]
 
+        # Queryset to grab all TaggedItems
+        # for the edited post.
         queryset = TaggedItem.objects \
             .get_tags_for(
                 Post,
                 instance.id
             )
 
+        # map with lambda to extract tag id & label
+        # from the queryset above.
         previous_tags = list(map(
             lambda tag: [
                 tag._state.fields_cache['tag'].id,
@@ -129,6 +154,8 @@ class PostSerializer(serializers.ModelSerializer):
         previous_tags.sort()
         validated_tags.sort()
 
+        # Code block checks previous tags against
+        # validated tags and acts accordingly.
         if len(validated_tags):
             for tag in previous_tags:
                 if tag[1] in validated_tags:
@@ -143,12 +170,14 @@ class PostSerializer(serializers.ModelSerializer):
                     .get(tag__label=tag[1])
                 TaggedItem.delete(remove_tag)
 
+        # Code block to check if new tags were added
+        # and acts accordingly.
         if 'add_tags' in validated_data:
             new_tags = list(map(
                 lambda tag: tag.lower(), validated_data['add_tags']
             ))
 
-            content_type_id = ContentType.objects.get(model="post").id
+            content_type = ContentType.objects.get(model="post")
 
             for label in new_tags:
                 tag = ""
@@ -159,7 +188,7 @@ class PostSerializer(serializers.ModelSerializer):
 
                 TaggedItem.objects.create(
                     object_id=instance.id,  # type: ignore
-                    content_type_id=content_type_id,  # type: ignore
+                    content_type=content_type,  # type: ignore
                     tag_id=tag.id  # type: ignore
                 )
 
@@ -167,6 +196,9 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Profile serializer extending profile user & post serializers.
+    """
     # https://www.django-rest-framework.org/api-guide/relations/#nested-relationships
     user = ProfileUserSerializer(read_only=True)
     profileposts = PostSerializer(many=True, read_only=True)
