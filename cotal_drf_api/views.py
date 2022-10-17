ce9f48b0f4633c50dd65f976a21aa381d02b3d7e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-10-17 12:40:47 +0200

    Refactor: remove http redirect and add api view to api start point

--- cotal_drf_api/views.py
+++ cotal_drf_api/views.py
@@ -1,12 +1,18 @@
-import os
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseRedirect
-
-
-def handle_redirect(request, *args, **kwargs):
-    return HttpResponseRedirect(settings.API_ENDPOINT)
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse
 
 
 def api_root(request):
     if request.method == 'GET':
         return HttpResponse('Welcome to Cotal API.')
+
+
+@api_view(['GET'])
+def api_start_endpoint(request):
+    if request.method == 'GET':
+        return Response({
+            'detail': 'Welcome to Cotal API. '
+            'From here you can query all known endpoints.'
+        })
