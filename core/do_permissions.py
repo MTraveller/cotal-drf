5753a7767b1055622d9e4e5685e048edb064746f
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-17 12:38:33 +0100

    Refactor: connected model permissions after lookup field changed to slug

--- core/do_permissions.py
+++ core/do_permissions.py
@@ -11,6 +11,7 @@ BASENAME_LIST = [
 
 BASENAME_DETAIL = [
     'profile-connecter-detail',
+    'profile-connecting-detail',
     'profile-follows-detail',
 ]
 
@@ -23,6 +24,7 @@ def try_match(self):
     kwargs = self.request.resolver_match.kwargs
 
     print("do_permissions")
+    print(basename)
     print(kwargs)
 
     if self.request.user.is_authenticated:
@@ -31,25 +33,50 @@ def try_match(self):
             return [IsAuthenticated()]
 
         if basename in BASENAME_LIST:
+            print(self.queryset)
             if not len(self.queryset) >= 1 \
                 and not \
                     (kwargs[
                         'profiles_slug'
                     ]) == self.request.user.profile.slug:
+                if basename == 'profile-connecting-list' and \
+                        len(self.queryset) == 0:
+                    return False
+
                 return [IsAuthenticated()]
+
             return False
 
         elif basename in BASENAME_DETAIL:
+            print("Hello BASENAME_DETAIL")
             try:
                 queryset_dict = self.queryset[0].__dict__
-                if 'connecter_id' in queryset_dict and \
-                        queryset_dict[
-                            'connecter_id'
-                        ] == self.request.user.id or \
-                        queryset_dict[
-                            'followed_by_id'
-                        ] == self.request.user.id:
+                print("queryset")
+                print(queryset_dict)
+                print(queryset_dict['connecting_id'])
+                print(self.request.user.id)
+                if basename == 'profile-connecter-detail' and \
+                    'connecter_id' in queryset_dict and \
+                        queryset_dict['connecter_id'] == self.request.user.id:
                     return [IsAuthenticated()]
+
+                elif basename == 'profile-connecting-detail' and \
+                    'connecting_id' in queryset_dict and \
+                        queryset_dict[
+                            'connecting_id'
+                        ] == self.request.user.id:
+                    print("If Connecting")
+                    return [IsAuthenticated()]
+
+                # elif 'connecting_id' in queryset_dict and \
+                #         queryset_dict[
+                #             'connecting_id'
+                #         ] == self.request.user.id or \
+                #         queryset_dict[
+                #             'followed_by_id'
+                #         ] == self.request.user.id:
+                #     print("TRY")
+                #     return [IsAuthenticated()]
             except IndexError:
                 pass
 
