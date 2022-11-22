01a87af60dd1aaa308e4f5fe99b612b2a233998b
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-22 16:14:38 +0100

    Refactor: remove try block no need query return empty string, remove profile in retrun statement

--- core/upload_to.py
+++ core/upload_to.py
@@ -9,22 +9,18 @@ def user_directory_path(instance, filename):
     """
     # Source:
     # https://docs.djangoproject.com/en/4.1/ref/models/fields/#django.db.models.FileField.upload_to
-    try:
-        """ try block automatically targets only 'PUT' requests """
-        previous_image = instance.__class__.objects.get(
-            id=instance.id).image
+    previous_image = instance.__class__.objects.get(
+        id=instance.id).image
 
+    if previous_image:
         media_uploaded.send_robust(
             instance.__class__, image=previous_image)
-
-    except:
-        pass
 
     instance_name = instance.__class__.__name__.lower()
 
     if instance_name == 'profile':
         return '{0}/{1}/{2}'.format(
-            instance.profile.slug, instance_name, filename
+            instance.slug, instance_name, filename
         )
 
     return '{0}/{1}/{2}'.format(
