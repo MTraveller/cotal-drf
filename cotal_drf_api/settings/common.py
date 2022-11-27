785afe37ec6395d0b50c8de4af5675c829333a6d
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-11-27 21:24:44 +0100

    Add: default parser classes for rest framework

--- cotal_drf_api/settings/common.py
+++ cotal_drf_api/settings/common.py
@@ -131,7 +131,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
 }
 
 AUTH_USER_MODEL = 'core.User'
