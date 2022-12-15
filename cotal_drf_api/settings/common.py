ae6f8d85c6bf54f6ee78622206dd544127abad18
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2022-12-15 15:28:39 +0100

    Add: static root and whitenoise middleware

--- cotal_drf_api/settings/common.py
+++ cotal_drf_api/settings/common.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Change MEDIA_URL value to your choosing, this will be reflected on
 # cloudinary e.g. <project-name>/<user_{id}>/sub-folders
